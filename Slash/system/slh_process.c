93091df049aaed950f36e339678fbd0c99701e2f
digital-pers0n <aquaphonic@gmail.com>
2018-07-23 23:09:09 +0300

    Add subprocess launcher

---
+++ Slash/system/slh_process.c
@@ -0,0 +1,9 @@
+//
+//  slh_process.c
+//  Slash
+//
+//  Created by Terminator on 2018/07/23.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#include "slh_process.h"
---
+++ Slash/system/slh_process.h
@@ -0,0 +1,14 @@
+//
+//  slh_process.h
+//  Slash
+//
+//  Created by Terminator on 2018/07/23.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_process_h
+#define slh_process_h
+
+#include <stdio.h>
+
+#endif /* slh_process_h */
