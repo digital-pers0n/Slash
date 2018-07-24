00b6d65ee82ec246b6bf9a972c405f25248e014a
digital-pers0n <aquaphonic@gmail.com>
2018-07-25 01:31:37 +0300

    Add slh_socket.c and slh_socket.h files

---
+++ Slash/system/slh_socket.c
@@ -0,0 +1,9 @@
+//
+//  slh_socket.c
+//  Slash
+//
+//  Created by Terminator on 2018/07/25.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#include "slh_socket.h"
---
+++ Slash/system/slh_socket.h
@@ -0,0 +1,15 @@
+//
+//  slh_socket.h
+//  Slash
+//
+//  Created by Terminator on 2018/07/25.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_socket_h
+#define slh_socket_h
+
+#include <stdio.h>
+
+
+#endif /* slh_socket_h */
