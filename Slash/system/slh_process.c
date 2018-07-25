6e87e8040520ad03cefe35b8b6187f6d5eb9956a
digital-pers0n <aquaphonic@gmail.com>
2018-07-25 21:15:08 +0300

    Implement initialization and destruction functions

--- Slash/system/slh_process.c
+++ Slash/system/slh_process.c
@@ -7,3 +7,30 @@
 //
 
 #include "slh_process.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <spawn.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <signal.h>
+
+#pragma mark - Initialization
+
+void prc_init(Process *p, char **args) {
+    p->args = args;
+    p->pid = -1;
+    p->std_err = NULL;
+    p->std_out = NULL;
+    p->fa = NULL;
+}
+
+#pragma mark - Destruction
+
+void prc_destroy(Process *p) {
+    if (prc_pid(p) > 0) {
+        prc_close(p);
+    }
+    p->args = NULL;
+}
