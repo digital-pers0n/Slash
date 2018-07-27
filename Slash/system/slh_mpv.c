9d7e207e7f0636eb35796fb7fe579bdf5c691751
digital-pers0n <aquaphonic@gmail.com>
2018-07-27 23:52:44 +0300

    Add slh_mpv source and header file

---
+++ Slash/system/slh_mpv.c
@@ -0,0 +1,9 @@
+//
+//  slh_mpv.c
+//  Slash
+//
+//  Created by Terminator on 2018/07/27.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#include "slh_mpv.h"
---
+++ Slash/system/slh_mpv.h
@@ -0,0 +1,14 @@
+//
+//  slh_mpv.h
+//  Slash
+//
+//  Created by Terminator on 2018/07/27.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_mpv_h
+#define slh_mpv_h
+
+#include <stdio.h>
+
+#endif /* slh_mpv_h */
