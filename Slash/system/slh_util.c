81982c431e66475774aa7a386d39e807c28cb011
digital-pers0n <aquaphonic@gmail.com>
2018-08-07 22:59:43 +0300

    Add slh_util.c and slh_util.h files

---
+++ Slash/system/slh_util.c
@@ -0,0 +1,9 @@
+//
+//  slh_util.c
+//  Slash
+//
+//  Created by Terminator on 2018/08/07.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#include "slh_util.h"
---
+++ Slash/system/slh_util.h
@@ -0,0 +1,14 @@
+//
+//  slh_util.h
+//  Slash
+//
+//  Created by Terminator on 2018/08/07.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_util_h
+#define slh_util_h
+
+#include <stdio.h>
+
+#endif /* slh_util_h */
