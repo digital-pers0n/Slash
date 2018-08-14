70dd1e8c5a9b248665c9e18665941ccc0ee76083
digital-pers0n <aquaphonic@gmail.com>
2018-08-14 21:14:17 +0300

    Add SLHPlayer class

---
+++ Slash/SLHPlayer.h
@@ -0,0 +1,13 @@
+//
+//  SLHPlayer.h
+//  Slash
+//
+//  Created by Terminator on 2018/08/14.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLHPlayer : NSObject
+
+@end
---
+++ Slash/SLHPlayer.m
@@ -0,0 +1,13 @@
+//
+//  SLHPlayer.m
+//  Slash
+//
+//  Created by Terminator on 2018/08/14.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHPlayer.h"
+
+@implementation SLHPlayer
+
+@end
