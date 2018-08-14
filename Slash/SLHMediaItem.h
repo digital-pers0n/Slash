401eb8e14810c96cf612635c899d9e7ae9d8d682
digital-pers0n <aquaphonic@gmail.com>
2018-08-14 21:19:42 +0300

    Add SLHMediaItem class

---
+++ Slash/SLHMediaItem.h
@@ -0,0 +1,13 @@
+//
+//  SLHMediaItem.h
+//  Slash
+//
+//  Created by Terminator on 2018/08/14.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLHMediaItem : NSObject
+
+@end
---
+++ Slash/SLHMediaItem.m
@@ -0,0 +1,13 @@
+//
+//  SLHMediaItem.m
+//  Slash
+//
+//  Created by Terminator on 2018/08/14.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHMediaItem.h"
+
+@implementation SLHMediaItem
+
+@end
