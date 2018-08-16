ca52b585237ed740295fbae944a2ee04bc82c422
digital-pers0n <aquaphonic@gmail.com>
2018-08-16 20:42:54 +0300

    Add SLHMediaItemTrack class

---
+++ Slash/SLHMediaItemTrack.h
@@ -0,0 +1,13 @@
+//
+//  SLHMediaItemTrack.h
+//  Slash
+//
+//  Created by Terminator on 2018/08/16.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLHMediaItemTrack : NSObject
+
+@end
---
+++ Slash/SLHMediaItemTrack.m
@@ -0,0 +1,13 @@
+//
+//  SLHMediaItemTrack.m
+//  Slash
+//
+//  Created by Terminator on 2018/08/16.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHMediaItemTrack.h"
+
+@implementation SLHMediaItemTrack
+
+@end
