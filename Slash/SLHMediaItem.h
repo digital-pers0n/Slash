86a7ab78bd4d48a194d492b3497c32f79fd55189
digital-pers0n <aquaphonic@gmail.com>
2018-08-17 21:10:03 +0300

    Add SLHMediaItem class interface

--- Slash/SLHMediaItem.h
+++ Slash/SLHMediaItem.h
@@ -8,6 +8,71 @@
 
 #import <Foundation/Foundation.h>
 
-@interface SLHMediaItem : NSObject
+NS_ASSUME_NONNULL_BEGIN
+
+/** 
+ * @class SLHMediaItem
+ *
+ * @abstract Media files representation.
+ */
+
+/**
+ * @enum SLHMediaItemStatus
+ * @abstract Indicate if an item can be successfully played.
+ *
+ * @constant SLHMediaItemStatusReadyToPlay
+ * Indicate that the item can be played.
+ *
+ * @constant SLHMediaItemStatusFailed
+ * Indicate that the item can no longer be played.
+ */
+typedef NS_ENUM(NSInteger, SLHMediaItemStatus) {
+    SLHMediaItemStatusReadyToPlay,
+    SLHMediaItemStatusFailed
+};
+
+@class SLHMediaItemTrack;
+@class SLHMetadataItem;
+
+@interface SLHMediaItem : NSObject <NSCopying>
+
+/**
+ * Create an instance of SLHMediaItem for playing a media file.
+ * @param path Media file path
+ * @return An instance of SLHMediaItem
+ */
++ (instancetype)mediaItemWithPath:(NSString *)path;
+
+/**
+ * Initialize an SLHMediaItem with a file path.
+ * @param path Media file path
+ * @return An instance of SLHMediaItem
+ */
+- (instancetype)initWithPath:(NSString *)path;
+
+/**
+ * Media item status.
+ */
+@property (readonly) SLHMediaItemStatus status;
+
+/**
+ * The error that caused the failure.
+ * @return if status is SLHMediaItemStatusFailed, return an instance of NSError or nil otherwise.
+ */
+@property (readonly, nullable) NSError *error;
+
+/**
+ * The tracks in a media file.
+ * @return An array of tracks the media file contains.
+ */
+@property (readonly) NSArray<SLHMediaItemTrack *> *tracks;
+
+/**
+ * Metadata stored by the media file.
+ * @return An array of SLHMetadataItem objects.
+ */
+@property (copy) NSArray<SLHMetadataItem *> *metadata;
 
 @end
+
+NS_ASSUME_NONNULL_END
