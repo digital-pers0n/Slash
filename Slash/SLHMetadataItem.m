0a6cbd641f6a999ae0074e64e2730f778de0f940
digital-pers0n <aquaphonic@gmail.com>
2018-08-18 19:17:19 +0300

    Add SLHMetadataItem class

---
+++ Slash/SLHMetadataItem.m
@@ -0,0 +1,13 @@
+//
+//  SLHMetadataItem.m
+//  Slash
+//
+//  Created by Terminator on 2018/08/18.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHMetadataItem.h"
+
+@implementation SLHMetadataItem
+
+@end
