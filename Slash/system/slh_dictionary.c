12a63541811fa0d66d5a23b16bdcb501394d7a0a
Terminator <user@imac.local>
2018-09-17 22:56:01 +0300

    Add slh_dictionary

---
+++ Slash/system/slh_dictionary.c
@@ -0,0 +1,9 @@
+//
+//  slh_dictionary.c
+//  Slash
+//
+//  Created by Terminator on 9/17/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#include "slh_dictionary.h"
---
+++ Slash/system/slh_dictionary.h
@@ -0,0 +1,14 @@
+//
+//  slh_dictionary.h
+//  Slash
+//
+//  Created by Terminator on 9/17/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_dictionary_h
+#define slh_dictionary_h
+
+#include <stdio.h>
+
+#endif /* slh_dictionary_h */
