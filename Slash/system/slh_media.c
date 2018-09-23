a0f9364426263daa0568b70529d629ed303bcabf
digital-pers0n <aquaphonic@gmail.com>
2018-09-23 22:55:55 +0300

    Add slh_media file

---
+++ Slash/system/slh_media.c
@@ -0,0 +1,9 @@
+//
+//  slh_media.c
+//  Slash
+//
+//  Created by Terminator on 9/23/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#include "slh_media.h"
---
+++ Slash/system/slh_media.h
@@ -0,0 +1,14 @@
+//
+//  slh_media.h
+//  Slash
+//
+//  Created by Terminator on 9/23/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_media_h
+#define slh_media_h
+
+#include <stdio.h>
+
+#endif /* slh_media_h */
