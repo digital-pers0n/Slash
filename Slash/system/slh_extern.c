314778a68bcda48e1a8a3aeb707211cec3496bce
digital-pers0n <aquaphonic@gmail.com>
2018-09-25 22:24:38 +0300

    Add slh_extern file

---
+++ Slash/system/slh_extern.c
@@ -0,0 +1,39 @@
+//
+//  slh_extern.c
+//  Slash
+//
+//  Created by Terminator on 9/25/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+
+/* Media Keys */
+const char *kMediaFilenameKey        = "filename";
+const char *kMediaFormatNameKey      = "format_name";
+const char *kMediaDurationKey        = "duration";
+const char *kMediaBitRateKey         = "bit_rate";
+const char *kMediaSizeKey            = "size";
+const char *kMediaStreamsKey         = "nb_streams";
+
+/* Stream Keys */
+const char *kMediaStreamIndexKey                = "index";
+const char *kMediaStreamCodecNameKey            = "codec_name";
+const char *kMediaStreamCodecTypeKey            = "codec_type";
+const char *kMediaStreamWidthKey                = "width";
+const char *kMediaStreamHeightKey               = "height";
+const char *kMediaStreamDisplayAspectRatioKey   = "display_aspect_ratio";
+const char *kMediaStreamPixFormatKey     = "pix_fmt";
+const char *kMediaStreamProfileKey       = "profile";
+const char *kMediaStreamDurationKey      = "duration";
+const char *kMediaStreamBitRateKey       = "bit_rate";
+const char *kMediaStreamMaxBitrateKey    = "max_bit_rate";
+
+const char *kMediaStreamSampleRateKey    = "sample_rate";
+const char *kMediaStreamChannelsKey      = "channels";
+const char *kMediaStreamChannelLayoutKey = "channel_layout";
+
+/* Metadata Keys */
+const char *kMediaMetadataTitleKey   = "TAG:title";
+const char *kMediaMetadataArtistKey  = "TAG:artist";
+const char *kMediaMetadataDateKey    = "TAG:date";
+const char *kMediaMetadataCommentKey = "TAG:comment";
