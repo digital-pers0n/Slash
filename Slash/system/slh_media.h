39c7a002d6782eacbe6811c7ac87b85f6b6dc37d
digital-pers0n <aquaphonic@gmail.com>
2018-09-25 22:19:06 +0300

    Add documentation

--- Slash/system/slh_media.h
+++ Slash/system/slh_media.h
@@ -26,13 +26,20 @@ typedef struct _Stream {
     Dictionary *info;
 } Stream;
 
+/**
+ * Get various information about the stream. 
+ * 
+ * @param key Stream key. Keys are declared in slh_media_keys.h. This parameter cannot be NULL.
+ *
+ * @return Value associated with the key or NULL.
+ */
 static inline char *stream_get_value(Stream *s, const char *key) {
     return dict_get_value(s->info, key);
-};
+}
 
 static inline CodecType stream_codec_type(Stream *s) {
     return s->codec_type;
-};
+}
 
 static inline unsigned int stream_index(Stream *s) {
     return s->index;
@@ -45,16 +52,40 @@ static inline Dictionary *stream_info(Stream *s) {
 typedef struct _Media {
     char *filename;
     char *format_name;
-    unsigned int nb_streams;
-    size_t size;
-    size_t bit_rate;
-    double duration;
+    unsigned int nb_streams;    // number of streams
+    size_t size;                // media size in bytes
+    size_t bit_rate;            // stream bit rate
+    double duration;            // stream duration in seconds
     Dictionary *info;
     Stream *streams;
 } Media;
 
+/**
+ * Initialize a media item.
+ *
+ * @param ffprobe_path Path to ffprobe executable. This parameter cannot be NULL.
+ *
+ * @param filename Path to a media file. This parameter cannot be NULL.
+ *
+ * @return 0 on success, otherwise -1 
+ */ 
 int media_init(Media *m, const char *ffprobe_path, const char *filename);
+
+/**
+ * Destroy the media item.
+ */
 void media_destroy(Media *m);
+
+/**
+ * Get metadata.
+ *
+ * @param key Metadata key. Keys are defined in slh_media_keys.h. This parameter cannot be NULL.
+ *
+ * @return Value associated with the key or NULL.
+ */
+static inline char *media_get_metadata(Media *m, const char *key) {
+    return dict_get_value(m->info, key);
+}
 
 static inline char *media_filename(Media *m) {
     return m->filename;
@@ -64,24 +95,40 @@ static inline char *media_format_name(Media *m) {
     return m->format_name;
 }
 
+/**
+ * Get number of streams.
+ */
 static inline unsigned int media_nb_streams(Media *m) {
     return m->nb_streams;
 }
 
+/**
+ * Get bit rate in bps.
+ */
 static inline size_t media_bit_rate(Media *m) {
     return m->bit_rate;
 }
 
+/**
+ * Get size in bytes.
+ */
 static inline size_t media_size(Media *m) {
     return m->size;
 }
 
+/**
+ * Get duration in seconds.
+ */
 static inline double media_duration(Media *m) {
     return m->duration;
 }
 
 static inline Stream *media_streams(Media *m) {
     return m->streams;
+}
+
+static inline Dictionary *media_info(Media *m) {
+    return m->info;
 }
 
 
