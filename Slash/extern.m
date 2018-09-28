1da636029863aa8ee96c1b2ad2fa940288adf22b
digital-pers0n <aquaphonic@gmail.com>
2018-09-28 23:53:32 +0300

    Add preferences keys

---
+++ Slash/extern.m
@@ -0,0 +1,15 @@
+//
+//  extern.m
+//  Slash
+//
+//  Created by Terminator on 9/28/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - SLHPreferences
+
+NSString *const SLHPreferencesFFMpegFilePathKey     = @"ffmpegBinary";
+NSString *const SLHPreferencesFFProbeFilePathKey    = @"ffprobeBinary";
+NSString *const SLHPreferencesMPVFIlePathKey        = @"mpvBinary";
