1f7b80fae7aa52c8191c5327e5402775966b51c3
digital-pers0n <aquaphonic@gmail.com>
2018-09-28 22:36:53 +0300

    Add files

---
+++ Slash/SLHPreferences.h
@@ -0,0 +1,13 @@
+//
+//  SLHPreferences.h
+//  Slash
+//
+//  Created by Terminator on 9/28/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHPreferences : NSWindowController
+
+@end
---
+++ Slash/SLHPreferences.m
@@ -0,0 +1,23 @@
+//
+//  SLHPreferences.m
+//  Slash
+//
+//  Created by Terminator on 9/28/18.
+//  Copyright © 2018 digital-pers0n. All rights reserved.
+//
+
+#import "SLHPreferences.h"
+
+@interface SLHPreferences ()
+
+@end
+
+@implementation SLHPreferences
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
