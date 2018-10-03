889dc1b164d55f41bca3ff8f7cdc8100f06abeac
digital-pers0n <aquaphonic@gmail.com>
2018-10-03 23:09:56 +0300

    Add methods

--- Slash/SLHMetadataItem.h
+++ Slash/SLHMetadataItem.h
@@ -8,6 +8,22 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ @class SLHMetadataItem
+ 
+ Metadata associated with a media file or its tracks
+*/
+
 @interface SLHMetadataItem : NSObject
+
+/**
+ Identfier of the metadata item.
+ */
+@property NSString *identifier;
+
+/**
+ value of the metadata item.
+ */
+@property NSString *value;
 
 @end
