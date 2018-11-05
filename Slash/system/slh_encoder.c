4f03a32229755813f8bc211db3e72c572892eb3c
digital-pers0n <aquaphonic@gmail.com>
2018-11-05 21:06:02 +0300

    Add files

---
+++ Slash/system/slh_encoder.c
@@ -0,0 +1,9 @@
+//
+//  slh_encoder.c
+//  Slash
+//
+//  Created by Terminator on 2018/11/05.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#include "slh_encoder.h"
---
+++ Slash/system/slh_encoder.h
@@ -0,0 +1,14 @@
+//
+//  slh_encoder.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/05.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_encoder_h
+#define slh_encoder_h
+
+#include <stdio.h>
+
+#endif /* slh_encoder_h */
