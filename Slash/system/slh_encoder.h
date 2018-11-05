4a008a02b903dd3fb56458cc7dbb140c94cde798
digital-pers0n <aquaphonic@gmail.com>
2018-11-05 21:18:08 +0300

    Add function prototypes

--- Slash/system/slh_encoder.h
+++ Slash/system/slh_encoder.h
@@ -10,5 +10,18 @@
 #define slh_encoder_h
 
 #include <stdio.h>
+#include "slh_process.h"
+
+typedef void (*encoder_callback_f)(char *data, void *context);
+
+typedef struct _Encoder {
+    Process *proc;
+    void *gr;           // dispatch_group
+} Encoder;
+
+int encoder_init(Encoder *enc, char *const *args);
+void encoder_destroy(Encoder *enc);
+int encoder_start(Encoder *enc, encoder_callback_f func, void *ctx);
+int encoder_stop(Encoder *enc);
 
 #endif /* slh_encoder_h */
