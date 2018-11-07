5ebca0b61b32daddd5b2a937b195a937fa00cf88
digital-pers0n <aquaphonic@gmail.com>
2018-11-07 20:23:04 +0300

    Add documentation

--- Slash/system/slh_encoder.h
+++ Slash/system/slh_encoder.h
@@ -19,9 +19,38 @@ typedef struct _Encoder {
     void *gr;           // dispatch_group
 } Encoder;
 
+/**
+ * Initialize an encoder.
+ *
+ * @param args a NULL-terminated array of pointers to NULL-terminated strings
+ *             args[0] must contain file path to ffmpeg executable.
+ *             This parameter cannot be NULL.
+ *
+ * @return 0 on success, -1 otherwise.
+ */
 int encoder_init(Encoder *enc, char *const *args);
+
+/**
+ * Destroy the encoder.
+ */
 void encoder_destroy(Encoder *enc);
+
+/**
+ * Start encoding.
+ *
+ * @param func Optional user-defined funciton.
+ *
+ * @param ctx Optional pointier to a user-defined context.
+ *
+ * @return 0 on success, -1 otherwise.
+ */
 int encoder_start(Encoder *enc, encoder_callback_f func, void *ctx);
+
+/**
+ * Stop encoding.
+ *
+ * @return 0 on success, -1 otherwise.
+ */
 int encoder_stop(Encoder *enc);
 
 #endif /* slh_encoder_h */
