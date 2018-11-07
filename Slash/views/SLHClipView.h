7fa2862e7fda40b5965243a29c9c28b7a724e881
digital-pers0n <aquaphonic@gmail.com>
2018-11-07 21:02:59 +0300

    Add files

---
+++ Slash/views/SLHClipView.h
@@ -0,0 +1,13 @@
+//
+//  SLHClipView.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/07.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHClipView : NSClipView
+
+@end
---
+++ Slash/views/SLHClipView.m
@@ -0,0 +1,17 @@
+//
+//  SLHClipView.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/07.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHClipView.h"
+
+@implementation SLHClipView
+
+- (BOOL)isFlipped {
+    return YES;
+}
+
+@end
