e87d1fd0152613fc907daebeb8115574687ca88d
digital-pers0n <aquaphonic@gmail.com>
2018-11-08 20:54:10 +0300

    Add files

---
+++ Slash/views/SLHTabBarView.h
@@ -0,0 +1,13 @@
+//
+//  SLHTabBarView.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/08.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHTabBarView : NSView
+
+@end
---
+++ Slash/views/SLHTabBarView.m
@@ -0,0 +1,19 @@
+//
+//  SLHTabBarView.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/08.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHTabBarView.h"
+
+@implementation SLHTabBarView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
