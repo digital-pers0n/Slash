8224ff192a9a2e7f2158cd0ce8a5f2fe410c860f
digital-pers0n <aquaphonic@gmail.com>
2018-11-13 20:08:23 +0300

    Add files

---
+++ Slash/controllers/SLHEncoderSettings.m
@@ -0,0 +1,22 @@
+//
+//  SLHEncoderSettings.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/13.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderSettings.h"
+
+@interface SLHEncoderSettings ()
+
+@end
+
+@implementation SLHEncoderSettings
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
