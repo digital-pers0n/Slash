786aac80158e3b0061bbdb9e6e1e268e2b5edbb4
digital-pers0n <aquaphonic@gmail.com>
2018-11-14 22:01:41 +0300

    Add files

---
+++ Slash/controllers/SLHMainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  SLHMainWindowController.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/14.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHMainWindowController : NSWindowController
+
+@end
---
+++ Slash/controllers/SLHMainWindowController.m
@@ -0,0 +1,23 @@
+//
+//  SLHMainWindowController.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/14.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHMainWindowController.h"
+
+@interface SLHMainWindowController ()
+
+@end
+
+@implementation SLHMainWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
