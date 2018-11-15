8005836224f4bac30df0940d78edb2052bb67a22
digital-pers0n <aquaphonic@gmail.com>
2018-11-15 21:54:20 +0300

    Add files

---
+++ Slash/model/SLHEncoderItem.h
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderItem.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/15.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLHEncoderItem : NSObject
+
+@end
---
+++ Slash/model/SLHEncoderItem.m
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderItem.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/15.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderItem.h"
+
+@implementation SLHEncoderItem
+
+@end
