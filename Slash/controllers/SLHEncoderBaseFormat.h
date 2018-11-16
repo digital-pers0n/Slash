60758e58730474babcacae9b1a139d47905827d0
digital-pers0n <aquaphonic@gmail.com>
2018-11-16 20:24:30 +0300

    Add files

---
+++ Slash/controllers/SLHEncoderBaseFormat.h
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderBaseFormat.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/16.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHEncoderBaseFormat : NSViewController
+
+@end
---
+++ Slash/controllers/SLHEncoderBaseFormat.m
@@ -0,0 +1,22 @@
+//
+//  SLHEncoderBaseFormat.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/16.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderBaseFormat.h"
+
+@interface SLHEncoderBaseFormat ()
+
+@end
+
+@implementation SLHEncoderBaseFormat
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
