5fe63a0a8b42e38d8d4cb934b71eb0279c215539
digital-pers0n <aquaphonic@gmail.com>
2018-11-19 23:42:58 +0300

    Add properties

--- Slash/model/SLHEncoderItem.h
+++ Slash/model/SLHEncoderItem.h
@@ -8,6 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+typedef struct time_interval {
+    double start;
+    double end;
+} TimeInterval;
+
+@class SLHMediaItem;
+
 @interface SLHEncoderItem : NSObject
 
+- (instancetype)initWithMediaItem:(SLHMediaItem *)item;
+
+@property (readonly) SLHMediaItem *mediaItem;
+
+@property TimeInterval interval;
+
+@property NSInteger videoStreamIndex;
+@property NSInteger audioStreamIndex;
+@property NSInteger subtitleStreamIndex;
+
 @end
+
+NS_ASSUME_NONNULL_END
