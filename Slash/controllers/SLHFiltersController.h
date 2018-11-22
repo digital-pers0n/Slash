9bc1a59ae0a3184202550660f552088873da0052
digital-pers0n <aquaphonic@gmail.com>
2018-11-22 22:09:16 +0300

    Add files

---
+++ Slash/controllers/SLHFiltersController.h
@@ -0,0 +1,13 @@
+//
+//  SLHFiltersController.h
+//  Slash
+//
+//  Created by Terminator on 2018/11/22.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHFiltersController : NSViewController
+
+@end
---
+++ Slash/controllers/SLHFiltersController.m
@@ -0,0 +1,22 @@
+//
+//  SLHFiltersController.m
+//  Slash
+//
+//  Created by Terminator on 2018/11/22.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHFiltersController.h"
+
+@interface SLHFiltersController ()
+
+@end
+
+@implementation SLHFiltersController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
