df8fe88e6e65c5b89dff6cca44766d749f7c00e3
digital-pers0n <aquaphonic@gmail.com>
2018-11-22 22:06:36 +0300

    Implement initialization methods

--- Slash/model/SLHEncoderItem.m
+++ Slash/model/SLHEncoderItem.m
@@ -7,7 +7,35 @@
 //
 
 #import "SLHEncoderItem.h"
+#import "SLHMediaItem.h"
 
 @implementation SLHEncoderItem
+
+#pragma mark - Initialize
+
+- (instancetype)initWithMediaItem:(SLHMediaItem *)item outputPath:(NSString *)outputMediaPath {
+    self = [super init];
+    if (self) {
+        _mediaItem = item;
+        _outputPath = outputMediaPath;
+        _subtitleStreamIndex = -1;
+        _twoPassEncoding = NO;
+        _videoOptions = @[].mutableCopy;
+        _videoFilters = @[].mutableCopy;
+        _audioOptions = @[].mutableCopy;
+        _audioFilters = @[].mutableCopy;
+        _firstPassOptions = @[].mutableCopy;
+        _metadata = @[].mutableCopy;
+    }
+    return self;
+}
+
+- (instancetype)initWithMediaItem:(SLHMediaItem *)item  {
+    NSString *path = item.filePath;
+    NSString *ext = path.pathExtension;
+    path = [path stringByDeletingPathExtension];
+    path = [NSString stringWithFormat:@"%@_%lu.%@", path, time(0), ext];
+    return [self initWithMediaItem:item outputPath:path];
+}
 
 @end
