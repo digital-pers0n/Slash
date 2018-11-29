9d600b465b100261d22989bcd3c0122661f17894
digital-pers0n <aquaphonic@gmail.com>
2018-11-29 19:26:29 +0300

    Initialize encoder settings

--- Slash/controllers/SLHMainWindowController.m
+++ Slash/controllers/SLHMainWindowController.m
@@ -8,9 +8,12 @@
 
 #import "SLHMainWindowController.h"
 #import "SLHDragView.h"
+#import "SLHEncoderSettings.h"
 
 @interface SLHMainWindowController () <SLHDragViewDelegate> {
     SLHDragView *_dragView;
+    SLHEncoderSettings *_encoderSettings;
+    IBOutlet NSView *_customView;
 }
 
 @end
@@ -23,12 +26,19 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
+    
+    /* SLHDragView */
     _dragView = [[SLHDragView alloc] init];
     _dragView.delegate = self;
     _dragView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
     _dragView.frame = self.window.contentView.frame;
-    
     [self.window.contentView addSubview:_dragView];
+    
+    /* SLHEncoderSettings */
+    _encoderSettings = [[SLHEncoderSettings alloc] init];
+    _encoderSettings.view.frame = _customView.frame;
+    _encoderSettings.view.autoresizingMask = _customView.autoresizingMask;
+    [_customView.superview replaceSubview:_customView with:_encoderSettings.view];
 }
 
 #pragma mark - SLHDragView Delegate
