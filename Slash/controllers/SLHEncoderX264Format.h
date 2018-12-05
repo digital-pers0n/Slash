47ea10e935ebeaebc725f9790286365a3bf47fb7
digital-pers0n <aquaphonic@gmail.com>
2018-12-05 22:25:08 +0300

    Add files

---
+++ Slash/controllers/SLHEncoderX264Format.h
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderX264Format.h
+//  Slash
+//
+//  Created by Terminator on 2018/12/05.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderBaseFormat.h"
+
+@interface SLHEncoderX264Format : SLHEncoderBaseFormat
+
+@end
---
+++ Slash/controllers/SLHEncoderX264Format.m
@@ -0,0 +1,22 @@
+//
+//  SLHEncoderX264Format.m
+//  Slash
+//
+//  Created by Terminator on 2018/12/05.
+//  Copyright © 2018年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderX264Format.h"
+
+@interface SLHEncoderX264Format ()
+
+@end
+
+@implementation SLHEncoderX264Format
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
