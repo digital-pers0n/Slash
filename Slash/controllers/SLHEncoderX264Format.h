2a8b04cbca4b67316cc2c8ed91b00f979ed60663
digital-pers0n <aquaphonic@gmail.com>
2018-12-06 22:18:38 +0300

    Add properties

--- Slash/controllers/SLHEncoderX264Format.h
+++ Slash/controllers/SLHEncoderX264Format.h
@@ -8,6 +8,57 @@
 
 #import "SLHEncoderBaseFormat.h"
 
+typedef NS_ENUM(NSUInteger, SLHX264EncodingType) {
+    SLHX264EncodingSinglePass,
+    SLHX264EncodingTwoPass,
+    SLHX264EncodingCRFSinglePass,
+};
+
+typedef NS_ENUM(NSUInteger, SLHX264PresetType) {
+    SLHX264PresetUltrafast,
+    SLHX264PresetSuperfast,
+    SLHX264PresetVeryfast,
+    SLHX264PresetFaster,
+    SLHX264PresetFast,
+    SLHX264PresetMedium,
+    SLHX264PresetSlow,
+    SLHX264PresetSlower,
+    SLHX264PresetVeryslow,
+    SLHX264PresetPlacebo,
+    SLHX264PresetNone = NSUIntegerMax
+};
+
+typedef NS_ENUM(NSUInteger, SLHX264ProfileType) {
+    SLHX264ProfileBaseline,
+    SLHX264ProfileMain,
+    SLHX264ProfileHigh,
+    SLHX264ProfileNone = NSUIntegerMax
+};
+
+typedef NS_ENUM(NSUInteger, SLHX264ContainerType) {
+    SLHX264ContainerMP4,
+    SLHX264ContainerM4V,
+    SLHX264ContainerMKV,
+    SLHX264ContainerMOV
+};
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface SLHEncoderX264Format : SLHEncoderBaseFormat
 
+@property SLHX264EncodingType encodingType;
+@property SLHX264PresetType presetType;
+@property SLHX264ProfileType profileType;
+@property NSUInteger videoWidth;
+@property NSUInteger videoHeight;
+
+/**
+ Comma-separated strings.
+ Valid strings: film animation grain stillimage psnr ssim fastdecode zerolatency
+ */
+@property NSString *tune;
+
+
 @end
+
+NS_ASSUME_NONNULL_END
