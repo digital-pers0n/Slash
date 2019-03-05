22bb90e90ba1980d92669e4d1dc76eae7ca31a60
digital-pers0n <aquaphonic@gmail.com>
2019-03-05 17:17:14 +0300

    Connect IB actions and outlets

--- Slash/controllers/SLHMetadataEditor.m
+++ Slash/controllers/SLHMetadataEditor.m
@@ -8,16 +8,37 @@
 
 #import "SLHMetadataEditor.h"
 
-@interface SLHMetadataEditor ()
+@interface SLHMetadataEditor () {
+    
+    IBOutlet NSTextView *_titleTextView;
+    IBOutlet NSTextView *_artistTextView;
+    IBOutlet NSTextView *_commentTextView;
+    IBOutlet NSTextField *_dateTextField;
+}
 
 @end
 
 @implementation SLHMetadataEditor
 
+- (NSString *)windowNibName {
+    return self.className;
+}
+
 - (void)windowDidLoad {
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+#pragma mark - IBActions
+
+- (IBAction)okButtonAction:(id)sender {
+}
+
+- (IBAction)cancelButtonAction:(id)sender {
+}
+
+- (IBAction)applyButtonAction:(id)sender {
 }
 
 @end
