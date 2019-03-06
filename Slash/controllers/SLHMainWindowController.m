9404f173cc651d7c623e6100528721f9f7f9cd43
digital-pers0n <aquaphonic@gmail.com>
2019-03-06 19:14:38 +0300

    Implement protocol methods

--- Slash/controllers/SLHMainWindowController.m
+++ Slash/controllers/SLHMainWindowController.m
@@ -13,12 +13,18 @@
 #import "SLHMediaItem.h"
 #import "SLHMediaItemTrack.h"
 #import "SLHPreferences.h"
+#import "SLHMetadataEditor.h"
+#import "SLHMetadataItem.h"
+#import "SLHMetadataIdentifiers.h"
 #import "SLHEncoderBaseFormat.h"
 #import "SLHEncoderX264Format.h"
 
-@interface SLHMainWindowController () <SLHDragViewDelegate> {
+@interface SLHMainWindowController () <SLHDragViewDelegate, SLHMetadataEditorDelegate, NSTableViewDelegate> {
     SLHDragView *_dragView;
     SLHEncoderSettings *_encoderSettings;
+    SLHMetadataEditor *_metadataEditor;
+    NSArray <SLHEncoderBaseFormat *> *_formats;
+    
     IBOutlet NSView *_customView;
     IBOutlet NSArrayController *_arrayController;
     IBOutlet NSTableView *_tableView;
@@ -71,8 +77,6 @@
     }
 }
 
-
-
 #pragma mark - NSTableView Delegate
 
 - (void)tableViewSelectionDidChange:(NSNotification *)notification {
@@ -81,6 +85,40 @@
     [self.window setTitleWithRepresentedFilename:item.mediaItem.filePath];
 }
 
+#pragma mark - SLHMetadataEditor Delegate
+
+- (NSMutableDictionary *)dataForMetadataEditor:(SLHMetadataEditor *)editor {
+    NSInteger row = _tableView.selectedRow;
+    SLHEncoderItem *item = _arrayController.arrangedObjects[row];
+    
+    NSMutableDictionary *data = item.metadata.mutableCopy;
+    if (data.count) {
+        return data;
+    } else {
+        NSArray *array = item.mediaItem.metadata;
+        for (SLHMetadataItem *m in array) {
+            if ([m.identifier isEqual:SLHMetadataIdentifierArtist]) {
+                data[SLHMetadataIdentifierArtist] = m.value;
+            } else if ([m.identifier isEqual:SLHMetadataIdentifierTitle]) {
+                data[SLHMetadataIdentifierTitle] = m.value;
+            } else if ([m.identifier isEqual:SLHMetadataIdentifierDate]) {
+                data[SLHMetadataIdentifierDate] = m.value;
+            } else if ([m.identifier isEqual:SLHMetadataIdentifierComment]) {
+                data[SLHMetadataIdentifierComment] = m.value;
+            }
+        }
+    }
+    if (!data.count) {  // No metadata
+        data[SLHMetadataIdentifierTitle] = item.mediaItem.filePath.lastPathComponent.stringByDeletingPathExtension;
+    }
+    return data;
+}
+
+- (void)metadataEditor:(SLHMetadataEditor *)editor didEndEditing:(NSMutableDictionary *)data {
+    NSInteger row = _tableView.selectedRow;
+    SLHEncoderItem *item = _arrayController.arrangedObjects[row];
+    item.metadata = data.mutableCopy;
+}
 
 #pragma mark - SLHDragView Delegate
 
@@ -118,6 +156,21 @@
 }
 
 #pragma mark - IBActions
+
+- (IBAction)showMetadataEditor:(id)sender {
+    
+    if (!_tableView.numberOfRows) { // empty table
+        NSBeep();
+        return;
+    }
+    
+    if (!_metadataEditor) {
+        _metadataEditor = [[SLHMetadataEditor alloc] init];
+        _metadataEditor.delegate = self;
+    }
+    [_metadataEditor showWindow:sender];
+    [_metadataEditor reloadData];
+}
 
 - (IBAction)addEncoderItem:(id)sender {
     
