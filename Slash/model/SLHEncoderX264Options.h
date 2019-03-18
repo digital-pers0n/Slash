9f0ca925f8df33b1ffe68737ce581d6c0de782dd
digital-pers0n <aquaphonic@gmail.com>
2019-03-18 19:15:07 +0300

    Add files

---
+++ Slash/model/SLHEncoderX264Options.h
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderX264Options.h
+//  Slash
+//
+//  Created by Terminator on 2019/03/18.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderItemOptions.h"
+
+@interface SLHEncoderX264Options : SLHEncoderItemOptions
+
+@end
---
+++ Slash/model/SLHEncoderX264Options.m
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderX264Options.m
+//  Slash
+//
+//  Created by Terminator on 2019/03/18.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderX264Options.h"
+
+@implementation SLHEncoderX264Options
+
+@end
