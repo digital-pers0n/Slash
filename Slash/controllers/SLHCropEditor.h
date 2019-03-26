597e723ea07e4cf4abd8ea32f4cd0f74419a3b98
digital-pers0n <aquaphonic@gmail.com>
2019-03-26 15:56:17 +0300

    Declare a new class

---
+++ Slash/controllers/SLHCropEditor.h
@@ -0,0 +1,13 @@
+//
+//  SLHCropEditor.h
+//  Slash
+//
+//  Created by Terminator on 2019/03/26.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHCropEditor : NSWindowController
+
+@end
---
+++ Slash/controllers/SLHCropEditor.m
@@ -0,0 +1,27 @@
+//
+//  SLHCropEditor.m
+//  Slash
+//
+//  Created by Terminator on 2019/03/26.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHCropEditor.h"
+
+@interface SLHCropEditor ()
+
+@end
+
+@implementation SLHCropEditor
+
+- (NSString *)windowNibName {
+    return self.className;
+}
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
