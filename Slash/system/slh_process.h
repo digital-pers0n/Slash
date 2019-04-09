21347982a206b50e0012df67a384c6671d6d2db5
digital-pers0n <aquaphonic@gmail.com>
2019-04-09 18:05:46 +0300

    Write documentation

--- Slash/system/slh_process.h
+++ Slash/system/slh_process.h
@@ -22,39 +22,91 @@ typedef struct _Process {
     posix_spawn_file_actions_t fa;
 } Process;
 
-/* Initialization / Destruction */
+/**
+ * Initialize a process.
+ *
+ * @param args a NULL-terminated array of pointers to NULL-terminated strings
+ *             args[0] must contain a path to an executable file.
+ *             This parameter cannot be NULL.
+ */
 void prc_init(Process *p, char *const *args);
 
+/**
+ * Destroy the process.
+ */
 void prc_destroy(Process *p);
 
-/* Launch a process and create stderr/stdout pipes. */
+/** 
+ * Launch the process and create stderr/stdout pipes. 
+ *
+ * @return 0 on success, -1 otherwise.
+ */
 int prc_launch(Process *p);
 
-/* Wait for the associated process to terminate, and return exit code. */
+/**
+ * Wait for the associated process to terminate.
+ *
+ * @return exit code of the process.
+ */
 int prc_close(Process *p);
 
-/* Kill the associated process. */
+/** 
+ * Kill the associated process. 
+ *
+ * @return 0 on success, -1 otherwise.
+ */
 int prc_kill(Process *p);
 
-/* Check if the associated process exists */
+/**
+ *  Check if the associated process exists.
+ *
+ *  @return 0 if the process is active.
+ */
 int prc_does_exist(Process *p);
 
 /* Accessors */
 
+/**
+ * Get the stdout pipe.
+ *
+ * @return stdout pipe or NULL.
+ */
 static inline FILE *prc_stdout(Process *p) {
     return p->std_out;
 }
 
+/**
+ * Get the stderr pipe.
+ *
+ * @return stderr pipe or NULL.
+ */
 static inline FILE *prc_stderr(Process *p) {
     return p->std_err;
 }
 
+/**
+ * Get the pid value of the running process.
+ *
+ * @return pid value or -1 if the process is not running.
+ */ 
 static inline pid_t prc_pid(Process *p) {
     return p->pid;
 }
 
+/**
+ * Set an array of arguments.
+ *
+ * @param args a NULL-terminated array of pointers to NULL-terminated strings
+ *             args[0] must contain a path to an executable file.
+ *             This parameter cannot be NULL.
+ */
 void prc_set_args(Process *p, char *const *args);
 
+/**
+ * Get the arguments array.
+ *
+ * @return arguments array.
+ */
 static inline char **prc_args(Process *p) {
     return p->args;
 }
