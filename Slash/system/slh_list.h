9403c13f74a5454d7f39c41415cbcc7472d6fbd4
digital-pers0n <aquaphonic@gmail.com>
2019-04-17 17:59:50 +0300

    Declare functions

--- Slash/system/slh_list.h
+++ Slash/system/slh_list.h
@@ -11,5 +11,67 @@
 
 #include <stdio.h>
 
+/*
+ * Linked list node
+ */
+
+typedef struct _ListNode {
+    void *data;
+    struct _ListNode *next;
+} ListNode;
+
+static inline void *list_node_data(ListNode *n) { return n->data; }
+static inline ListNode *list_node_next(ListNode *n) { return n->next; }
+
+
+/*
+ * Linked list
+ */
+
+typedef struct _List {
+    size_t size;
+    void (*destroy)(void *data);    // data destructor
+    
+    ListNode *head;
+    ListNode *tail;
+} List;
+
+void list_init(List *list, void (*destroy)(void *data));
+void list_destroy(List *list);
+int list_insert_next(List *list, ListNode *element, const void *data);
+int list_remove_next(List *list, ListNode *element, void **data);
+static inline size_t list_size(List *list) { return list->size; }
+static inline ListNode *list_head(List *list) { return list->head; }
+static inline ListNode *list_tail(List *list) { return list->tail; }
+static inline int list_is_head(List *list, ListNode *node) { return (list->head == node); }
+static inline int list_is_tail(ListNode *node) { return (node->next == NULL); }
+
+
+/*
+ * Queue
+ */
+
+typedef List Queue;
+
+static inline void queue_init(Queue *q, void (*destroy)(void *data)) {
+    list_init(q, destroy);
+}
+
+static inline void queue_destroy(Queue *q) {
+    list_destroy(q);
+}
+
+static inline int queue_enqueue(Queue *q, void *data) {
+    return (list_insert_next(q, list_tail(q), data));
+}
+
+static inline int queue_dequeue(Queue *q, void **data) {
+    return (list_remove_next(q, NULL, data));
+}
+
+static inline size_t queue_size(Queue *q) {
+    return list_size(q);
+}
+
 
 #endif /* slh_list_h */
