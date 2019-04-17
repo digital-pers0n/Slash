52fd6d5b20d489884906c6a5a7f5c02b621400eb
digital-pers0n <aquaphonic@gmail.com>
2019-04-17 17:57:12 +0300

    Add files

---
+++ Slash/system/slh_list.c
@@ -0,0 +1,9 @@
+//
+//  slh_list.c
+//  Slash
+//
+//  Created by Terminator on 2019/04/17.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#include "slh_list.h"
---
+++ Slash/system/slh_list.h
@@ -0,0 +1,15 @@
+//
+//  slh_list.h
+//  Slash
+//
+//  Created by Terminator on 2019/04/17.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#ifndef slh_list_h
+#define slh_list_h
+
+#include <stdio.h>
+
+
+#endif /* slh_list_h */
