4a83666a73565d82755728e05431856cd5dc8f70
digital-pers0n <aquaphonic@gmail.com>
2019-04-18 17:01:28 +0300

    Add functions to dealloc data

--- Slash/system/slh_list.c
+++ Slash/system/slh_list.c
@@ -6,6 +6,7 @@
 //  Copyright © 2019年 digital-pers0n. All rights reserved.
 //
 
+#include <stdlib.h>
 #include "slh_list.h"
 
 #pragma mark - Initialization / Destruction
@@ -17,4 +18,29 @@ void list_init(List *l, void (*destroy)(void *data)) {
     l->tail = NULL;
 }
 
+static inline void _list_free_data(List *l) {
+    while (list_size(l) > 0) {
+        l->destroy_f(l->head->data);
+        ListNode *old_node = l->head;
+        l->head = l->head->next;
+        free(old_node);
+        l->size--;
+    }
+}
 
+static inline void _list_no_free(List *l) {
+    while (list_size(l) > 0) {
+        ListNode *old_node = l->head;
+        l->head = l->head->next;
+        free(old_node);
+        l->size--;
+    }
+}
+
+void list_destroy(List *l) {
+    if (l->destroy_f == NULL) {
+        _list_no_free(l);
+    } else {
+        _list_free_data(l);
+    }
+}
