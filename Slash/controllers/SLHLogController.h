ab90534173e1a8663259ace64775cbb3cf893ec2
digital-pers0n <aquaphonic@gmail.com>
2019-05-10 16:20:21 +0300

    Add files

---
+++ Slash/controllers/SLHLogController.h
@@ -0,0 +1,13 @@
+//
+//  SLHLogController.h
+//  Slash
+//
+//  Created by Terminator on 2019/05/10.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHLogController : NSWindowController
+
+@end
---
+++ Slash/controllers/SLHLogController.m
@@ -0,0 +1,23 @@
+//
+//  SLHLogController.m
+//  Slash
+//
+//  Created by Terminator on 2019/05/10.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHLogController.h"
+
+@interface SLHLogController ()
+
+@end
+
+@implementation SLHLogController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
