7b1e84eab620b7d772346a0758aa04f61ba84022
digital-pers0n <aquaphonic@gmail.com>
2019-05-14 18:40:13 +0300

    Add files

---
+++ Slash/controllers/SLHArgumentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLHArgumentsViewController.h
+//  Slash
+//
+//  Created by Terminator on 2019/05/14.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHArgumentsViewController : NSViewController
+
+@end
---
+++ Slash/controllers/SLHArgumentsViewController.m
@@ -0,0 +1,26 @@
+//
+//  SLHArgumentsViewController.m
+//  Slash
+//
+//  Created by Terminator on 2019/05/14.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHArgumentsViewController.h"
+
+@interface SLHArgumentsViewController ()
+
+@end
+
+@implementation SLHArgumentsViewController
+
+- (NSString *)nibName {
+    return self.className;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
