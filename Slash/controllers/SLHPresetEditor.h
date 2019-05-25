473397a986437c27ceee54a87a80002e8285502e
digital-pers0n <aquaphonic@gmail.com>
2019-05-25 18:30:06 +0300

    Add files

---
+++ Slash/controllers/SLHPresetEditor.h
@@ -0,0 +1,13 @@
+//
+//  SLHPresetEditor.h
+//  Slash
+//
+//  Created by Terminator on 2019/05/25.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SLHPresetEditor : NSWindowController
+
+@end
---
+++ Slash/controllers/SLHPresetEditor.m
@@ -0,0 +1,25 @@
+//
+//  SLHPresetEditor.m
+//  Slash
+//
+//  Created by Terminator on 2019/05/25.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHPresetEditor.h"
+
+@interface SLHPresetEditor ()
+
+@end
+
+@implementation SLHPresetEditor
+
+- (NSString *)windowNibName {
+    return self.className;
+}
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+}
+
+@end
