fa9043a1a0ab68817cae1270d0c45dc279050494
digital-pers0n <aquaphonic@gmail.com>
2019-06-02 17:00:06 +0300

    Remove unneeded code

--- Slash/AppDelegate.m
+++ Slash/AppDelegate.m
@@ -11,11 +11,6 @@
 #import "SLHPreferences.h"
 #import "SLHMediaItem.h"
 
-static NSString *const _ffmpegPath  = @"/usr/local/bin/ffmpeg1";
-static NSString *const _ffprobePath = @"/usr/local/bin/ffprobe";
-static NSString *const _mpvPath     = @"/usr/local/bin/mpv1";
-static NSString *const _appInitializedKey = @"appInitialized";
-
 char *g_temp_dir;
 
 @interface AppDelegate ()
@@ -48,40 +43,6 @@ char *g_temp_dir;
      g_temp_dir = strdup(tmpDir.UTF8String);
     [_mainWindow showWindow:self];
     _preferences = [SLHPreferences preferences];
-    NSUserDefaults *defs = [NSUserDefaults standardUserDefaults];
-    if (![defs boolForKey:_appInitializedKey]) {
-        NSFileManager *fm = [NSFileManager defaultManager];
-        NSMutableString *missing = [NSMutableString new];
-        if ([fm fileExistsAtPath:_ffmpegPath isDirectory:NO]) {
-            [defs setObject:_ffmpegPath forKey:SLHPreferencesFFMpegFilePathKey];
-        } else {
-            [missing appendString:@"ffmpeg\n"];
-        }
-    
-        if ([fm fileExistsAtPath:_ffprobePath isDirectory:NO]) {
-            [defs setObject:_ffprobePath forKey:SLHPreferencesFFProbeFilePathKey];
-        } else {
-            [missing appendString:@"ffprobe\n"];
-        }
-        
-        if ([fm fileExistsAtPath:_mpvPath isDirectory:NO]) {
-            [defs setObject:_mpvPath forKey:SLHPreferencesMPVFilePathKey];
-        } else {
-            [missing appendString:@"mpv\n"];
-        }
-        
-        if (missing.length) {
-            NSAlert *alert = [[NSAlert alloc] init];
-            alert.messageText = @"Error";
-            alert.informativeText =
-                    [NSString stringWithFormat:@"Following components are missing:\n\n%@\n"
-                                                "Install them in /urs/local/bin/ or set valid paths in preferences", missing];
-            [alert runModal];
-        } else {
-            [defs setBool:YES forKey:_appInitializedKey];
-        }
-    }
-    
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
