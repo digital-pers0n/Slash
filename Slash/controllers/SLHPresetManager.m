479cf1fed4d2bcbc8afdf303368de717b7347901
digital-pers0n <aquaphonic@gmail.com>
2019-06-12 18:26:54 +0300

    Rename class

---
+++ Slash/controllers/SLHPresetManager.m
@@ -0,0 +1,25 @@
+//
+//  SLHPresetEditor.m
+//  Slash
+//
+//  Created by Terminator on 2019/05/25.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHPresetManager.h"
+
+@interface SLHPresetManager ()
+
+@end
+
+@implementation SLHPresetManager
+
+- (NSString *)windowNibName {
+    return self.className;
+}
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+}
+
+@end
