87d4a051de999afed96a1725b3bae83ea3dfd90f
digital-pers0n <aquaphonic@gmail.com>
2019-06-28 16:50:51 +0300

    Add files

---
+++ Slash/controllers/SLHEncoderUniversalFormat.h
@@ -0,0 +1,13 @@
+//
+//  SLHEncoderUniversalFormat.h
+//  Slash
+//
+//  Created by Terminator on 2019/06/28.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderBaseFormat.h"
+
+@interface SLHEncoderUniversalFormat : SLHEncoderBaseFormat
+
+@end
---
+++ Slash/controllers/SLHEncoderUniversalFormat.m
@@ -0,0 +1,26 @@
+//
+//  SLHEncoderUniversalFormat.m
+//  Slash
+//
+//  Created by Terminator on 2019/06/28.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHEncoderUniversalFormat.h"
+
+@interface SLHEncoderUniversalFormat ()
+
+@end
+
+@implementation SLHEncoderUniversalFormat
+
+- (NSString *)nibName {
+    return self.className;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
