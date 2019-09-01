c127a66c76a3d53001a4ec5e86699f2821d834f4
digital-pers0n <aquaphonic@gmail.com>
2019-09-01 18:01:59 +0300

    Add files

---
+++ Slash/controllers/SLHWindowController.h
@@ -0,0 +1,17 @@
+//
+//  SLHWindowController.h
+//  Slash
+//
+//  Created by Terminator on 2019/09/01.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLHWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/controllers/SLHWindowController.m
@@ -0,0 +1,27 @@
+//
+//  SLHWindowController.m
+//  Slash
+//
+//  Created by Terminator on 2019/09/01.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHWindowController.h"
+
+@interface SLHWindowController ()
+
+@end
+
+@implementation SLHWindowController
+
+- (NSString *)windowNibName {
+    return self.className;
+}
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
