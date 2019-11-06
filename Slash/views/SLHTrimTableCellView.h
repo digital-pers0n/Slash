0b0000df99dcfeeb9c784b1027bf1cc908af485c
digital-pers0n <aquaphonic@gmail.com>
2019-11-06 17:21:50 +0300

    Add subclass of NSTableCellView

---
+++ Slash/views/SLHTrimTableCellView.h
@@ -0,0 +1,21 @@
+//
+//  SLHTrimTableCellView.h
+//  Slash
+//
+//  Created by Terminator on 2019/11/06.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class SLHTrimView;
+
+@interface SLHTrimTableCellView : NSTableCellView
+
+@property (nonatomic, nullable) IBOutlet SLHTrimView *trimView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/views/SLHTrimTableCellView.m
@@ -0,0 +1,42 @@
+//
+//  SLHTrimTableCellView.m
+//  Slash
+//
+//  Created by Terminator on 2019/11/06.
+//  Copyright © 2019年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHTrimTableCellView.h"
+#import "SLHTrimView.h"
+#import "SLHEncoderItem.h"
+#import "MPVPlayerItem.h"
+
+@implementation SLHTrimTableCellView
+
+- (void)setObjectValue:(id)objectValue {
+    if (!objectValue) { return; }
+    SLHEncoderItem *obj = objectValue;
+    
+    [_trimView bind:@"startValue" toObject:obj withKeyPath:@"intervalStart" options:nil];
+    [_trimView bind:@"endValue" toObject:obj withKeyPath:@"intervalEnd" options:nil];
+    [_trimView bind:@"maxValue" toObject:obj withKeyPath:@"playerItem.duration" options:nil];
+ 
+    _trimView.maxValue = obj.playerItem.duration;
+    _trimView.startValue = obj.intervalStart;
+    _trimView.endValue = obj.intervalEnd;
+}
+
+- (void)dealloc {
+
+    [_trimView unbind:@"startValue"];
+    [_trimView unbind:@"endValue"];
+    [_trimView unbind:@"maxValue"];
+}
+
+- (void)prepareForReuse {
+
+    [_trimView prepareForReuse];
+    [super prepareForReuse];
+}
+
+@end
