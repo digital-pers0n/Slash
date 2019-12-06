c6b2f6511a2fe5b2b29a3b2058cf518324d49f22
digital-pers0n <aquaphonic@gmail.com>
2019-12-06 19:13:39 +0300

    Use default global constants as binding names

--- Slash/views/SLHTrimTableCellView.m
+++ Slash/views/SLHTrimTableCellView.m
@@ -21,8 +21,8 @@
     [_trimView bind:@"maxValue" toObject:obj withKeyPath:@"playerItem.duration" options:nil];
         [_trimView bind:@"endValue" toObject:obj withKeyPath:@"intervalEnd" options:nil];
     [_trimView bind:@"startValue" toObject:obj withKeyPath:@"intervalStart" options:nil];
-    [_outNameTextField bind:@"stringValue" toObject:obj withKeyPath:@"outputFileName" options:nil];
-    [_outNameTextField bind:@"toolTip" toObject:obj withKeyPath:@"outputPath" options:nil];
+    [_outNameTextField bind:NSValueBinding toObject:obj withKeyPath:@"outputFileName" options:nil];
+    [_outNameTextField bind:NSToolTipBinding toObject:obj withKeyPath:@"outputPath" options:nil];
 
     _trimView.maxValue = obj.playerItem.duration;
     _trimView.endValue = obj.intervalEnd;
@@ -37,8 +37,8 @@
     [_trimView unbind:@"startValue"];
     [_trimView unbind:@"endValue"];
     [_trimView unbind:@"maxValue"];
-    [_outNameTextField unbind:@"stringValue"];
-    [_outNameTextField unbind:@"toolTip"];
+    [_outNameTextField unbind:NSValueBinding];
+    [_outNameTextField unbind:NSToolTipBinding];
 }
 
 - (void)prepareForReuse {
