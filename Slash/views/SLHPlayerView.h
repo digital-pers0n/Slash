88e878779cddb2601a7282d0b37f52af98d634e5
digital-pers0n <aquaphonic@gmail.com>
2019-12-15 17:55:27 +0300

    Remove unused

--- Slash/views/SLHPlayerView.h
+++ Slash/views/SLHPlayerView.h
@@ -12,20 +12,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class MPVPlayer;
 
-typedef NS_ENUM(NSInteger, MPVPlayerViewControlsStyle) {
-    MPVPlayerViewControlsStyleNone,
-    MPVPlayerViewControlsStyleInline,
-    MPVPlayerViewControlsStyleFloating,
-    MPVlayerViewControlsStyleDefault = MPVPlayerViewControlsStyleInline
-};
-
 @interface SLHPlayerView : NSView
 
 @property (nullable) MPVPlayer *player;
-@property MPVPlayerViewControlsStyle controlsStyle;
 @property (getter=isReadyForDisplay, readonly) BOOL readyForDisplay;
 
 @end
-
 
 NS_ASSUME_NONNULL_END
