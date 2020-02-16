6edb68411a648f89ab6d7f770d80cdcfca1914db
digital-pers0n <aquaphonic@gmail.com>
2020-02-16 19:05:22 +0300

    Add FadingTableCellView class

---
+++ Slash/views/SLHFadingTableCellView.h
@@ -0,0 +1,19 @@
+//
+//  SLHFadingTableCellView.h
+//  Slash
+//
+//  Created by Terminator on 2020/02/16.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLHFadingTableCellView : NSTableCellView
+
+@property (nonatomic, weak) IBOutlet NSButton *fadingButton;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/views/SLHFadingTableCellView.m
@@ -0,0 +1,46 @@
+//
+//  SLHFadingTableCellView.m
+//  Slash
+//
+//  Created by Terminator on 2020/02/16.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHFadingTableCellView.h"
+
+@interface SLHFadingTableCellView () {
+    NSTrackingArea *_trackingArea;
+}
+@end
+
+@implementation SLHFadingTableCellView
+
+- (void)awakeFromNib {
+    _trackingArea = [NSTrackingArea new];
+    _fadingButton.alphaValue = 0.0;
+    _fadingButton.wantsLayer = YES;
+}
+
+- (void)updateTrackingAreas {
+    [super updateTrackingAreas];
+    [self removeTrackingArea:_trackingArea];
+    
+    _trackingArea = [[NSTrackingArea alloc] initWithRect:NSZeroRect
+                                                 options:
+                     NSTrackingInVisibleRect |
+                     NSTrackingActiveInKeyWindow |
+                     NSTrackingMouseEnteredAndExited
+                                                   owner:self
+                                                userInfo:nil];
+    [self addTrackingArea:_trackingArea];
+}
+
+- (void)mouseEntered:(NSEvent *)event {
+    _fadingButton.animator.alphaValue = 1.0;
+}
+
+- (void)mouseExited:(NSEvent *)event {
+    _fadingButton.animator.alphaValue = 0.0;
+}
+
+@end
