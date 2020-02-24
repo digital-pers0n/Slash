942d5237f345998fcaf8f021a530ae748797b2ac
digital-pers0n <aquaphonic@gmail.com>
2020-02-24 20:44:50 +0300

    Add lightweight separator view class, because default one in the IB is not customizable

---
+++ Slash/views/SLHSeparatorView.h
@@ -0,0 +1,20 @@
+//
+//  SLHSeparatorView.h
+//  Slash
+//
+//  Created by Terminator on 2020/02/24.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+IB_DESIGNABLE
+@interface SLHSeparatorView : NSView
+
+@property (nonatomic) IBInspectable NSColor *color;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/views/SLHSeparatorView.m
@@ -0,0 +1,47 @@
+//
+//  SLHSeparatorView.m
+//  Slash
+//
+//  Created by Terminator on 2020/02/24.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHSeparatorView.h"
+#define ENABLE_1PIX_SEPARATOR 0
+
+
+@implementation SLHSeparatorView
+
+- (void)awakeFromNib {
+    if (!_color) {
+        _color = [NSColor windowFrameColor];
+    }
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [_color set];
+    NSRect frame;
+    
+#if ENABLE_1PIX_SEPARATOR
+    
+    frame = self.frame;
+    NSSize cellSize = NSMakeSize(NSWidth(frame), 1);
+    
+    frame.origin.y = round((NSHeight(frame) - cellSize.height) * (CGFloat)0.5);
+    frame.origin.x = round((NSWidth(frame) - cellSize.width) * (CGFloat)0.5);
+    frame.size = cellSize;
+    
+#else
+    
+    frame = dirtyRect;
+    
+#endif
+    
+     [NSBezierPath fillRect:frame];
+}
+
+- (BOOL)isOpaque {
+    return YES;
+}
+
+@end
