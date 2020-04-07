c41cb1be92271105f8194c771a20eb33093caeec
digital-pers0n <aquaphonic@gmail.com>
2020-04-07 21:27:27 +0300

    Add TrimViewSettings class

---
+++ Slash/SLHTrimViewSettings.h
@@ -0,0 +1,21 @@
+//
+//  SLHTrimViewSettings.h
+//  Slash
+//
+//  Created by Terminator on 2020/04/07.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class SLHTrimViewController;
+
+@interface SLHTrimViewSettings : NSViewController
+
+@property (nonatomic, weak, nullable) SLHTrimViewController * controller;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/SLHTrimViewSettings.m
@@ -0,0 +1,22 @@
+//
+//  SLHTrimViewSettings.m
+//  Slash
+//
+//  Created by Terminator on 2020/04/07.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import "SLHTrimViewSettings.h"
+
+@interface SLHTrimViewSettings ()
+
+@end
+
+@implementation SLHTrimViewSettings
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
