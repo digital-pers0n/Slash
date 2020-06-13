0f31d90233939e86d6dbe9a321437b38bc91cda9
digital-pers0n <aquaphonic@gmail.com>
2020-06-13 18:11:29 +0300

    Allow to enable/disable the output name text field

--- Slash/controllers/SLHOutputNameController.h
+++ Slash/controllers/SLHOutputNameController.h
@@ -14,6 +14,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, weak) IBOutlet NSArrayController *encoderItemsArrayController;
 
+/** Indicate if the output file name is editable. */
+@property (nonatomic) BOOL nameEditable;
+
 @end
 
 NS_ASSUME_NONNULL_END
