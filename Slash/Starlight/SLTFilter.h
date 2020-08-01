f74a4b43f6fcb61203b91d0d5f30b1711776a5b2
digital-pers0n <aquaphonic@gmail.com>
2020-08-01 20:57:02 +0300

    Implement SLTFilter class

--- Slash/Starlight/SLTFilter.h
+++ Slash/Starlight/SLTFilter.h
@@ -10,7 +10,41 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SLTFilter : NSObject
+typedef NS_ENUM(NSInteger, SLTFilterKind) {
+    SLTFilterKindVideo,
+    SLTFilterKindAudio,
+};
+
+@class SLTFilterParameter;
+
+@interface SLTFilter : NSObject <NSCopying>
+
+- (instancetype)initWithFilterName:(NSString *)filterName
+                       displayName:(NSString *)displayName
+                              kind:(SLTFilterKind)kind;
+
+/** FFmpeg filter name. */
+@property (nonatomic) NSString *filterName;
+
+/** Display name to use with UI elements. */
+@property (nonatomic) NSString *displayName;
+
+/** Determine if it's -af or -vf filter. */
+@property (nonatomic) SLTFilterKind kind;
+
+/** Indicate if the filter should be used. */
+@property (nonatomic) BOOL enabled;
+
+/** Array of filter parameters. */
+@property (nonatomic) NSArray <SLTFilterParameter *> *parameters;
+
+/** 
+ Return a string to use with ffmpeg -vf or -af command line args.
+ The string is constructed from elements inside the @c parameters array e.g.
+ 'filter=key1=value1:key2=value2'.
+ If the filter doesn't have any paramters, return only the filterName.
+ */
+@property (nonatomic, readonly) NSString *stringValue;
 
 @end
 
--- Slash/Starlight/SLTFilter.m
+++ Slash/Starlight/SLTFilter.m
@@ -7,7 +7,63 @@
 //
 
 #import "SLTFilter.h"
+#import "SLTFilterParameter.h"
 
 @implementation SLTFilter
+
+- (id)copyWithZone:(NSZone *)zone {
+    SLTFilter *filter = [[self.class allocWithZone:zone] init];
+    filter->_filterName = _filterName.copy;
+    filter->_displayName = _displayName.copy;
+    filter->_enabled = _enabled;
+    filter->_kind = _kind;
+    filter->_parameters = _parameters.copy;
+    return filter;
+}
+
+- (instancetype)initWithFilterName:(NSString *)filterName
+                       displayName:(NSString *)displayName
+                              kind:(SLTFilterKind)kind
+{
+    self = [super init];
+    if (self) {
+        _filterName = filterName;
+        _displayName = displayName;
+        _kind = kind;
+        _parameters = @[];
+    }
+    return self;
+}
+
+- (instancetype)init {
+    return [self initWithFilterName:@"" displayName:@"" kind:SLTFilterKindVideo];
+}
+
+- (NSString *)stringValue {
+   
+    NSUInteger count = _parameters.count;
+    NSString *string = nil;
+    switch (count) {
+        case 0:
+            return _filterName;
+            break;
+            
+        case 1:
+            string = [[_parameters objectAtIndex:0] stringValue];
+            break;
+            
+        default:
+        {
+            NSMutableArray *strings = [NSMutableArray array];
+            
+            for (SLTFilterParameter *param in _parameters) {
+                [strings addObject:param.stringValue];
+            }
+            string = [strings componentsJoinedByString:@":"];
+        }
+            break;
+    }
+    return [NSString stringWithFormat:@"%@=%@", _filterName, string];
+}
 
 @end
