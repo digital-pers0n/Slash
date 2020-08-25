663fdc3a66c951d7d3186aefa9e9393e21ee144c
digital-pers0n <aquaphonic@gmail.com>
2020-08-25 20:24:36 +0300

    Add copyWithZone:

--- Slash/Starlight/SLTEncoderSettings.m
+++ Slash/Starlight/SLTEncoderSettings.m
@@ -7,7 +7,19 @@
 //
 
 #import "SLTEncoderSettings.h"
+#import "SLTMediaSettings.h"
 
 @implementation SLTEncoderSettings
+
+- (id)copyWithZone:(NSZone *)zone {
+    SLTEncoderSettings *obj = [[self.class allocWithZone:zone] init];
+    obj->_enableAudioPassThrough = _enableAudioPassThrough;
+    obj->_enableVideoPassThrough = _enableVideoPassThrough;
+    obj->_videoSettings = _videoSettings.copy;
+    obj->_audioSettings = _audioSettings.copy;
+    obj->_containerName = _containerName.copy;
+    obj->_allowsTwoPassEncoding = _allowsTwoPassEncoding;
+    return obj;
+}
 
 @end
