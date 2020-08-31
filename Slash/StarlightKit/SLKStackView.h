b324d52cc2bb56d17c14ef7292e9d36932eaa70e
digital-pers0n <aquaphonic@gmail.com>
2020-08-31 21:30:21 +0300

    Add files

---
+++ Slash/StarlightKit/SLKStackView.h
@@ -0,0 +1,17 @@
+//
+//  SLKStackView.h
+//  Slash
+//
+//  Created by Terminator on 2020/8/31.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLKStackView : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/StarlightKit/SLKStackView.m
@@ -0,0 +1,17 @@
+//
+//  SLKStackView.m
+//  Slash
+//
+//  Created by Terminator on 2020/8/31.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import "SLKStackView.h"
+
+@implementation SLKStackView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+}
+
+@end
