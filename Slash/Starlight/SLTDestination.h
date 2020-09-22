2b67f3d74bd3a8b5f903c06afd623a96077e298e
digital-pers0n <aquaphonic@gmail.com>
2020-09-22 19:13:45 +0300

    Add accessors

--- Slash/Starlight/SLTDestination.h
+++ Slash/Starlight/SLTDestination.h
@@ -10,7 +10,26 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface SLTDestination : NSObject
+@class SLTEncoderSettings, SLTFilter;
+
+@interface SLTDestination : NSObject <NSCopying>
+
+/** Access the output file path. */
+@property (nonatomic) NSString *filePath;
+
+/** Same as filePath.lastPathComponent */
+@property (nonatomic) NSString *fileName;
+
+@property (nonatomic) SLTEncoderSettings *settings;
+@property (nonatomic) NSArray<SLTFilter *> *videoFilters;
+@property (nonatomic) NSArray<SLTFilter *> *audioFilters;
+@property (nonatomic) NSDictionary<NSString *, NSString *> *metadata;
+
+/** Set the in point in seconds. */
+@property (nonatomic) CGFloat inPoint;
+
+/** Set the out point in seconds. */
+@property (nonatomic) CGFloat outPoint;
 
 @end
 
