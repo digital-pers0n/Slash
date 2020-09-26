f1d86431e516bf0b3184defbcb5041d4a74e9e43
digital-pers0n <aquaphonic@gmail.com>
2020-09-26 19:14:39 +0300

    Implement NSCopying protocol

--- Slash/Starlight/SLTDestination.m
+++ Slash/Starlight/SLTDestination.m
@@ -7,7 +7,29 @@
 //
 
 #import "SLTDestination.h"
+#import "SLTEncoderSettings.h"
 
 @implementation SLTDestination
+
+- (id)copyWithZone:(NSZone *)zone {
+    typeof(self) obj = [[self.class allocWithZone:zone] init];
+    obj->_filePath = _filePath.copy;
+    obj->_settings = _settings.copy;
+    obj->_videoFilters = _videoFilters.copy;
+    obj->_audioFilters = _audioFilters.copy;
+    obj->_metadata = _metadata.copy;
+    obj->_inPoint = _inPoint;
+    obj->_outPoint = _outPoint;
+    return obj;
+}
+
+- (void)setFileName:(NSString *)fileName {
+    id path = _filePath.stringByDeletingLastPathComponent;
+    self.filePath = [path stringByAppendingPathComponent:fileName];
+}
+
+- (NSString *)fileName {
+    return _filePath.lastPathComponent;
+}
 
 @end
