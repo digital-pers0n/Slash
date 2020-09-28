19159226a5591699b87fcb21640b67a6f029b8e5
digital-pers0n <aquaphonic@gmail.com>
2020-09-28 20:46:44 +0300

    Add files

---
+++ Slash/Starlight/SLTTask.h
@@ -0,0 +1,22 @@
+//
+//  SLTTask.h
+//  Slash
+//
+//  Created by Terminator on 2020/9/28.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class SLTSource, SLTDestination;
+
+@interface SLTTask : NSObject
+
+@property (nonatomic) SLTSource *source;
+@property (nonatomic) SLTDestination *destination;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/Starlight/SLTTask.m
@@ -0,0 +1,13 @@
+//
+//  SLTTask.m
+//  Slash
+//
+//  Created by Terminator on 2020/9/28.
+//  Copyright © 2020年 digital-pers0n. All rights reserved.
+//
+
+#import "SLTTask.h"
+
+@implementation SLTTask
+
+@end
