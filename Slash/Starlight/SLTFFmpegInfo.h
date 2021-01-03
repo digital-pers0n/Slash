ab3f65739850ea96681e4eda323e26cdbf6bb704
digital-pers0n <aquaphonic@gmail.com>
2021-01-03 19:23:22 +0300

    Implement FFmpegInfo class

--- Slash/Starlight/SLTFFmpegInfo.h
+++ Slash/Starlight/SLTFFmpegInfo.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SLTFFmpegInfo : NSObject
 
-- (nullable instancetype)initWithPath:(NSString *)path error:(NSError **)error;
+- (nullable instancetype)initWithPath:(NSString *)path error:(out NSError **)error;
 @property (nonatomic, readonly) NSString *path;
 @property (nonatomic, readonly) NSArray *supportedCodecs;
 @property (nonatomic, readonly) NSArray *supportedFilters;
