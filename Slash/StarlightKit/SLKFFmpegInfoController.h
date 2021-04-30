6c9edbb5a1ba2de8d6a7ec50e9f78454a16eba44
digital-pers0n <aquaphonic@gmail.com>
2021-04-30 22:54:51 +0300

    Add SLKFFmpegInfoController class

---
+++ Slash/StarlightKit/SLKFFmpegInfoController.h
@@ -0,0 +1,17 @@
+//
+//  SLKFFmpegInfoController.h
+//  Slash
+//
+//  Created by Terminator on 2021/4/30.
+//  Copyright © 2021年 digital-pers0n. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SLKFFmpegInfoController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Slash/StarlightKit/SLKFFmpegInfoController.mm
@@ -0,0 +1,23 @@
+//
+//  SLKFFmpegInfoController.mm
+//  Slash
+//
+//  Created by Terminator on 2021/4/30.
+//  Copyright © 2021年 digital-pers0n. All rights reserved.
+//
+
+#import "SLKFFmpegInfoController.h"
+
+@interface SLKFFmpegInfoController ()
+
+@end
+
+@implementation SLKFFmpegInfoController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
